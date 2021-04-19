b7080ad03539f09943862ede66dcb3ec421bdbe0
knav777-brightvessel <kleinmann@brightvessel.com>
2021-04-19 05:12:48 -0400

    fix-script-styles-add

--- dogs/wp-content/plugins/knav777/knav777.php
+++ dogs/wp-content/plugins/knav777/knav777.php
@@ -19,9 +19,17 @@ defined('ABSPATH') or die();
 class Bootstrap{
 
     const POST_TYPE_BOOK = 'book';
+    const SCRIPTS_JS = 'knav777-scripts-js';
+    const STYLES_CSS = 'knav777-styles-css';
 
     public function __construct(){
         add_action('init', [$this, 'custom_post_type']);
+
+        add_action('admin_enqueue_scripts', [$this, 'registerStyles']);
+        add_action('admin_enqueue_scripts', [$this, 'registerScripts']);
+
+        add_action('wp_enqueue_scripts', [$this, 'registerStyles']);
+        add_action('wp_enqueue_scripts', [$this, 'registerScripts']);
     }
 
     public function activatePlugin(){
@@ -36,7 +44,15 @@ class Bootstrap{
     }
 
     public function custom_post_type(){
-        register_post_type(Bootstrap::POST_TYPE_BOOK, ['public' => true, 'label' => 'Books']);
+        register_post_type(self::POST_TYPE_BOOK, ['public' => true, 'label' => 'Books']);
+    }
+
+    public function registerScripts(){
+        wp_enqueue_script(self::SCRIPTS_JS, plugins_url('/assets/js/main.js', __FILE__));
+    }
+
+    public function registerStyles(){
+        wp_enqueue_style(self::STYLES_CSS, plugins_url('/assets/css/main.css', __FILE__));
     }
 }
 
